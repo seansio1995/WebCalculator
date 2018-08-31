3fff1a8346dd03113f8054b784cfaaa88346e557
seansio1995 <seansio@163.com>
2018-08-31 12:49:50 -0700

    finish

--- src/org/me/WebCalculator.java
+++ src/org/me/WebCalculator.java
@@ -3,6 +3,7 @@ package org.me;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import java.lang.Math;
 
 /**
  * This class acts as the web service based calculator
@@ -24,6 +25,7 @@ public class WebCalculator {
 		return param;
 	}
 	
+	
 	/**
 	 * This method adds two integers
 	 * 
@@ -36,5 +38,82 @@ public class WebCalculator {
 		// Add a and b
 		int sum = a + b;
 		return sum;
+	}
+	
+	
+	
+	/**
+	 * This method subtracts two integers
+	 * 
+	 * @param a the first integer
+	 * @param b the second integer
+	 * @return the sum of a and b
+	 */
+	@WebMethod(action = "Subtract")
+	public int subtract(@WebParam(name = "a") int a, @WebParam(name = "b") int b) {
+		// Add a and b
+		int res = a - b;
+		return res;
+	}
+	
+	/**
+	 * This method multiply two integers
+	 * 
+	 * @param a the first integer
+	 * @param b the second integer
+	 * @return the sum of a and b
+	 */
+	@WebMethod(action = "Multiply")
+	public int multiply(@WebParam(name = "a") int a, @WebParam(name = "b") int b) {
+		// Add a and b
+		int res = a * b;
+		return res;
+	}
+	
+	
+	/**
+	 * This method adds two integers
+	 * 
+	 * @param a the first integer
+	 * @param b the second integer
+	 * @return the sum of a and b
+	 */
+	@WebMethod(action = "Divide")
+	public int divide(@WebParam(name = "a") int a, @WebParam(name = "b") int b) {
+		// Add a and b
+		int res = a / b;
+		return res;
+	}
+	
+	
+	
+	/**
+	 * This method adds two integers
+	 * 
+	 * @param a the first integer
+	 * @param b the second integer
+	 * @return the sum of a and b
+	 */
+	@WebMethod(action = "Modulo")
+	public int mod(@WebParam(name = "a") int a, @WebParam(name = "b") int b) {
+		// Add a and b
+		int res = a % b;
+		return res;
+	}
+	
+	
+	
+	/**
+	 * This method adds two integers
+	 * 
+	 * @param a the first integer
+	 * @param b the second integer
+	 * @return the sum of a and b
+	 */
+	@WebMethod(action = "Power")
+	public double power(@WebParam(name = "a") int a, @WebParam(name = "b") int b) {
+		// Add a and b
+		double pow = Math.pow(a,b);
+		return pow;
 	}
 }
